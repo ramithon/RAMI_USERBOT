4da1cb9b18deb616e5643debaf96bf9f4bfcd555
ramithon <96627803+ramithon@users.noreply.github.com>
2022-01-01 01:31:43 +0200

    Update _an.py

--- resources/Formation/animation/_an.py
+++ resources/Formation/animation/_an.py
@@ -7,7 +7,7 @@ from userbot.Config import Config
 
 USERID = Config.OWNER_ID 
 ALIVE_NAME = Config.ALIVE_NAME
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Zed Userbot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "RAMBO Userbot"
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 
 #  ======================================================= #
