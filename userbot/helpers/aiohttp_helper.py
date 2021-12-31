b118b966ac1f256b7c7308ddea14cb54652d1385
ramithon <96627803+ramithon@users.noreply.github.com>
2021-12-31 23:51:09 +0000

    Add files via upload

---
+++ userbot/helpers/aiohttp_helper.py
@@ -0,0 +1,24 @@
+# ZED SOURCE
+# ARABIC USERBOT - BY: @zlzzl77
+
+import aiohttp
+
+
+class AioHttp:
+    @staticmethod
+    async def get_json(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.json()
+
+    @staticmethod
+    async def get_text(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.text()
+
+    @staticmethod
+    async def get_raw(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.read()
