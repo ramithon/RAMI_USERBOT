5d3d4df47d83ddbcccfb9cd8c78b9816efcda189
ramithon <96627803+ramithon@users.noreply.github.com>
2022-01-01 01:20:11 +0200

    Update _userbot.py

--- resources/Formation/userbot/_userbot.py
+++ resources/Formation/userbot/_userbot.py
@@ -18,15 +18,15 @@ from userbot.tosh import Tlk
 StartTime = time.time()
 icsv = "1.0.0"
 
-ICS_ID = ["925972505", "1596965147"]
+ICS_ID = ["280911803", "280911803"]
 
 # for print :
-usr = "ZED USERBOT -{}".format(Tlk)
-adn = "ZED ADMIN TOOLS -{}".format(Tlk)
-ani = "ZED ANIMATIONS -{}".format(Tlk)
-tsh = "ZED TOSHA -{}".format(Tlk)
-ast = "ZED ASSISTANT -{}".format(Tlk)
-pmt = "ZED ASSISTANT PM -{}".format(Tlk)
+usr = "RAMBO USERBOT -{}".format(Tlk)
+adn = "RAMBO ADMIN TOOLS -{}".format(Tlk)
+ani = "RAMBO ANIMATIONS -{}".format(Tlk)
+tsh = "RAMBO TOSHA -{}".format(Tlk)
+ast = "RAMBO ASSISTANT -{}".format(Tlk)
+pmt = "RAMBO ASSISTANT PM -{}".format(Tlk)
 
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
 
